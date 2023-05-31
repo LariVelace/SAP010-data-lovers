168eaffa4317f4b683a4bc95c94655e1c747b327
LariVelace <131285148+LariVelace@users.noreply.github.com>
2023-05-31 18:45:57 -0300

    trazendo nome,numero e imagem no cardDetails

--- src/cardDetails.js
+++ src/cardDetails.js
@@ -11,3 +11,10 @@ const pokemon = data.pokemon.filter((pk) => {
 console.log(id);
 console.log(pokemon);
 
+const pokemonName = document.getElementById("pokemonName");
+const pokemonNumber = document.getElementById("pokemonNumber");
+const pokemonImg = document.querySelector("#cardImg img");
+
+pokemonName.innerHTML = pokemon.name;
+pokemonNumber.innerHTML = pokemon.num;
+pokemonImg.src = pokemon.img;
